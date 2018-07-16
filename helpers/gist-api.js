6075bb358ccea68b7baa25dfb549514a4ca3c5b0
Farhan Ansari <farhan7015@gmail.com>
2018-07-16 16:42:33 +0530

    Helper> gitst-api.js updated

--- helpers/gist-api.js
+++ helpers/gist-api.js
@@ -1,113 +1,114 @@
 function getUserGists(user, callback) {
-  var requestUrl = 'https://api.github.com/users/' + user + '/gists';
+	var requestUrl = 'https://api.github.com/users/' + user + '/gists';
 
-  $.ajax({
-    url: requestUrl,
-    dataType: 'json'
-  }).done(function(gists){
-    callback(gists);
-  }).fail(function(error){
-    callback(null, error);
-  });
-}
-function getGist(gistId, callback) {
-  var requestUrl = 'https://api.github.com/gists/' + gistId;
-
-  $.ajax({
-    url: requestUrl,
-    dataType: 'json'
-  }).done(function(gist){
-    callback(gist);
-  }).fail(function(error){
-    callback(null, error);
-  });
+	$.ajax({
+		url: requestUrl,
+		dataType: 'json'
+	}).done(function (gists) {
+		callback(gists);
+	}).fail(function (error) {
+		callback(null, error);
+	});
 }
 
-function buildBodyContent(gist, callback) {
-  // Find the first non-truncated file in the gist
-  // and use it.
-  for (var filename in gist.files) {
-    if (gist.files.hasOwnProperty(filename)) {
-      var file = gist.files[filename];
-      if (!file.truncated) {
-        // We have a winner
-        switch (file.language) {
-          case 'HTML':
-            // Insert as-is
-            callback(file.content);
-            break;
-          case 'Markdown':
-            // Convert Markdown to HTML
-            var converter = new showdown.Converter();
-            var html = converter.makeHtml(file.content);
-            callback(html);
-            break;
-          default:
-            // Insert contents as a <code> block
-            var codeBlock = '<pre><code>';
-            codeBlock = codeBlock + file.content;
-            codeBlock = codeBlock + '</code></pre>';
-            callback(codeBlock);
-        }
-        return;
-      }
-    }
-  }
-  callback(null, 'No suitable file found in the gist');
-}
 function buildGistList(parent, gists, clickFunc) {
-  gists.forEach(function(gist, index) {
+	gists.forEach(function (gist, index) {
 
-    var listItem = $('<li/>')
-      .addClass('ms-ListItem')
-      .addClass('is-selectable')
-      .attr('tabindex', index)
-      .appendTo(parent);
+		var listItem = $('<li/>')
+			.addClass('ms-ListItem')
+			.addClass('is-selectable')
+			.attr('tabindex', index)
+			.appendTo(parent);
 
-    var desc = $('<span/>')
-      .addClass('ms-ListItem-primaryText')
-      .text(gist.description)
-      .appendTo(listItem);
+		var desc = $('<span/>')
+			.addClass('ms-ListItem-primaryText')
+			.text(gist.description)
+			.appendTo(listItem);
 
-    var desc = $('<span/>')
-      .addClass('ms-ListItem-secondaryText')
-      .text(buildFileList(gist.files))
-      .appendTo(listItem);
+		var desc = $('<span/>')
+			.addClass('ms-ListItem-secondaryText')
+			.text(buildFileList(gist.files))
+			.appendTo(listItem);
 
-    var updated = new Date(gist.updated_at);
+		var updated = new Date(gist.updated_at);
 
-    var desc = $('<span/>')
-      .addClass('ms-ListItem-tertiaryText')
-      .text('Last updated ' + updated.toLocaleString())
-      .appendTo(listItem);
+		var desc = $('<span/>')
+			.addClass('ms-ListItem-tertiaryText')
+			.text('Last updated ' + updated.toLocaleString())
+			.appendTo(listItem);
 
-    var selTarget = $('<div/>')
-      .addClass('ms-ListItem-selectionTarget')
-      .appendTo(listItem);
+		var selTarget = $('<div/>')
+			.addClass('ms-ListItem-selectionTarget')
+			.appendTo(listItem);
 
-    var id = $('<input/>')
-      .addClass('gist-id')
-      .attr('type', 'hidden')
-      .val(gist.id)
-      .appendTo(listItem);
-  });
+		var id = $('<input/>')
+			.addClass('gist-id')
+			.attr('type', 'hidden')
+			.val(gist.id)
+			.appendTo(listItem);
+	});
 
-  $('.ms-ListItem').on('click', clickFunc);
+	$('.ms-ListItem').on('click', clickFunc);
 }
 
 function buildFileList(files) {
 
-  var fileList = '';
+	var fileList = '';
 
-  for (var file in files) {
-    if (files.hasOwnProperty(file)) {
-      if (fileList.length > 0) {
-        fileList = fileList + ', ';
-      }
+	for (var file in files) {
+		if (files.hasOwnProperty(file)) {
+			if (fileList.length > 0) {
+				fileList = fileList + ', ';
+			}
 
-      fileList = fileList + files[file].filename + ' (' + files[file].language + ')';
-    }
-  }
+			fileList = fileList + files[file].filename + ' (' + files[file].language + ')';
+		}
+	}
 
-  return fileList;
+	return fileList;
+}
+function getGist(gistId, callback) {
+	var requestUrl = 'https://api.github.com/gists/' + gistId;
+
+	$.ajax({
+		url: requestUrl,
+		dataType: 'json'
+	}).done(function (gist) {
+		callback(gist);
+	}).fail(function (error) {
+		callback(null, error);
+	});
+}
+
+function buildBodyContent(gist, callback) {
+	// Find the first non-truncated file in the gist
+	// and use it.
+	for (var filename in gist.files) {
+		if (gist.files.hasOwnProperty(filename)) {
+			var file = gist.files[filename];
+			if (!file.truncated) {
+				// We have a winner, chicken dinner
+				switch (file.language) {
+					case 'HTML':
+						// Insert as-is
+						callback(file.content);
+						break;
+					case 'Markdown':
+						// Convert Markdown to HTML
+						var converter = new showdown.Converter();
+						var html = converter.makeHtml(file.content);
+						callback(html);
+						break;
+					default:
+						// Insert contents as a <code> block
+						var codeBlock = '<pre><code>';
+						codeBlock = codeBlock + file.content;
+						codeBlock = codeBlock + '</code></pre>';
+						callback(codeBlock);
+				}
+				return;
+			}
+		}
+	}
+	callback(null, 'No suitable file found in the gist');
 }
