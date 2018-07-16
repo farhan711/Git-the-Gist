84776caea56000b5a6ba665cb1f73afee92717e0
Farhan Ansari <farhan7015@gmail.com>
2018-07-16 16:18:08 +0530

    Helpers> gist-api.js Updated
    Functions Added

--- helpers/gist-api.js
+++ helpers/gist-api.js
@@ -10,7 +10,51 @@ function getUserGists(user, callback) {
     callback(null, error);
   });
 }
+function getGist(gistId, callback) {
+  var requestUrl = 'https://api.github.com/gists/' + gistId;
 
+  $.ajax({
+    url: requestUrl,
+    dataType: 'json'
+  }).done(function(gist){
+    callback(gist);
+  }).fail(function(error){
+    callback(null, error);
+  });
+}
+
+function buildBodyContent(gist, callback) {
+  // Find the first non-truncated file in the gist
+  // and use it.
+  for (var filename in gist.files) {
+    if (gist.files.hasOwnProperty(filename)) {
+      var file = gist.files[filename];
+      if (!file.truncated) {
+        // We have a winner
+        switch (file.language) {
+          case 'HTML':
+            // Insert as-is
+            callback(file.content);
+            break;
+          case 'Markdown':
+            // Convert Markdown to HTML
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(file.content);
+            callback(html);
+            break;
+          default:
+            // Insert contents as a <code> block
+            var codeBlock = '<pre><code>';
+            codeBlock = codeBlock + file.content;
+            codeBlock = codeBlock + '</code></pre>';
+            callback(codeBlock);
+        }
+        return;
+      }
+    }
+  }
+  callback(null, 'No suitable file found in the gist');
+}
 function buildGistList(parent, gists, clickFunc) {
   gists.forEach(function(gist, index) {
 
