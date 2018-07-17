11254be73b9a6526419c2f1bfbcca2eedaacfd9e
Farhan Ansari <farhan7015@gmail.com>
2018-07-17 12:00:05 +0530

    Helper> Files updated

--- helpers/addin-config.js
+++ helpers/addin-config.js
@@ -1,15 +1,15 @@
 function getConfig() {
-  var config = {};
+	var config = {};
 
-  config.gitHubUserName = Office.context.roamingSettings.get('gitHubUserName');
-  config.defaultGistId = Office.context.roamingSettings.get('defaultGistId');
+	config.gitHubUserName = Office.context.roamingSettings.get('gitHubUserName');
+	config.defaultGistId = Office.context.roamingSettings.get('defaultGistId');
 
-  return config;
+	return config;
 }
 
 function setConfig(config, callback) {
-  Office.context.roamingSettings.set('gitHubUserName', config.gitHubUserName);
-  Office.context.roamingSettings.set('defaultGistId', config.defaultGistId);
+	Office.context.roamingSettings.set('gitHubUserName', config.gitHubUserName);
+	Office.context.roamingSettings.set('defaultGistId', config.defaultGistId);
 
-  Office.context.roamingSettings.saveAsync(callback);
+	Office.context.roamingSettings.saveAsync(callback);
 }
